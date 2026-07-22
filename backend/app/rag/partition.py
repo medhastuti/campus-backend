c960c806bed92a9dd877819739a94fce858b7f47
Medha <medhastuti@gmail.com>
2026-07-23 01:38:59 +0530

    Update partition.py

--- backend/app/rag/partition.py
+++ backend/app/rag/partition.py
@@ -7,7 +7,7 @@ def partition_document(file_path: str):
     
     elements = partition_pdf(
         filename=file_path,  # Path to your PDF file
-        strategy="hi_res", # Use the most accurate (but slower) processing method of extraction
+        strategy="fast", # Use the most accurate (but slower) processing method of extraction
         infer_table_structure=True, # Keep tables as structured HTML, not jumbled text
         extract_image_block_types=["Image"], # Grab images found in the PDF
         extract_image_block_to_payload=True # Store images as base64 data you can actually use
